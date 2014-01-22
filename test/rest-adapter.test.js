0c994c7f9df4ce719cbb503a1c1903a904b4e5dd
Miroslav Bajtoš <miroslav@strongloop.com>
2014-01-22 19:09:02 +0100

    Clean up RestAdapter, add RestClass and RestMethod
    Simplify the code building up the REST handler. Bits that can be reused
    are moved to two new classes - RestAdapter and RestMethod.
    
    These two new classes should contain all information needed to generate
    front-end code (Angular $resource factory, Swagger descriptors, etc.).

---
+++ test/rest-adapter.test.js
@@ -0,0 +1,212 @@
+var extend = require('util')._extend;
+var inherits = require('util').inherits;
+var RemoteObjects = require('../');
+var RestAdapter = require('../lib/rest-adapter');
+var SharedClass = require('../lib/shared-class');
+var SharedMethod = require('../lib/shared-method');
+var expect = require('chai').expect;
+var factory = require('./helpers/shared-objects-factory.js');
+
+describe('RestAdapter', function() {
+  var remotes;
+
+  beforeEach(function() {
+    remotes = RemoteObjects.create();
+  });
+
+
+  describe('getClasses()', function() {
+    it('fills `name`', function() {
+      remotes.exports.testClass = factory.createSharedClass();
+      var classes = getRestClasses();
+      expect(classes[0]).to.have.property('name', 'testClass');
+    });
+
+    it('fills `routes`', function() {
+      remotes.exports.testClass = factory.createSharedClass();
+      remotes.exports.testClass.http = { path: '/test-class', verb: 'any' };
+
+      var classes = getRestClasses();
+
+      expect(classes[0]).to.have.property('routes')
+        .eql([{ path: '/test-class', verb: 'any' }]);
+    });
+
+    it('fills `ctor`', function() {
+      var testClass = remotes.exports.testClass = factory.createSharedClass();
+      testClass.sharedCtor.http = { path: '/shared-ctor', verb: 'all' };
+
+      var classes = getRestClasses();
+
+      expect(classes[0].ctor).to.have.property('routes')
+        .eql([{ path: '/shared-ctor', verb: 'all' }]);
+    });
+
+    it('fills static methods', function() {
+      var testClass = remotes.exports.testClass = factory.createSharedClass();
+      testClass.staticMethod = extend(someFunc, { shared: true });
+
+      var methods = getRestClasses()[0].methods;
+
+      expect(methods).to.have.length(1);
+      expect(methods[0]).to.have.property('name', 'staticMethod');
+      expect(methods[0]).to.have.property('fullName', 'testClass.staticMethod');
+      expect(methods[0])
+        .to.have.deep.property('routes[0].path', '/staticMethod');
+    });
+
+    it('fills prototype methods', function() {
+      var testClass = remotes.exports.testClass = factory.createSharedClass();
+      testClass.prototype.instanceMethod = extend(someFunc, { shared: true });
+
+      var methods = getRestClasses()[0].methods;
+
+      expect(methods).to.have.length(1);
+      expect(methods[0])
+        .to.have.property('fullName', 'testClass.prototype.instanceMethod');
+      expect(methods[0])
+        // Note: the `/id:` part is coming from testClass.sharedCtor
+        .to.have.deep.property('routes[0].path', '/:id/instanceMethod');
+    });
+
+    function getRestClasses() {
+      return new RestAdapter(remotes).getClasses();
+    }
+  });
+
+  describe('RestClass', function() {
+    describe('getPath', function() {
+      it('returns the path of the first route', function() {
+        var restClass = givenRestClass({ http: [
+          { path: '/a-path' },
+          { path: '/another-path' }
+        ]});
+        expect(restClass.getPath()).to.equal('/a-path');
+      });
+    });
+
+    function givenRestClass(config) {
+      var ctor = factory.createSharedClass(config);
+      remotes.testClass = ctor;
+      var sharedClass = new SharedClass('testClass', ctor);
+      return new RestAdapter.RestClass(sharedClass);
+    }
+  });
+
+  describe('RestMethod', function() {
+    var anArg = { arg: 'an-arg-name', type: String };
+
+    it('has `accepts`', function() {
+      var method = givenRestStaticMethod({ accepts: anArg });
+      expect(method.accepts).to.eql([anArg]);
+    });
+
+    it('has `returns`', function() {
+      var method = givenRestStaticMethod({ returns: anArg });
+      expect(method.returns).to.eql([anArg]);
+    });
+
+    it('has `description`', function() {
+      var method = givenRestStaticMethod({ description: 'a-desc' });
+      expect(method.description).to.equal('a-desc');
+    });
+
+    describe('isReturningArray()', function() {
+      it('returns true when there is single root Array arg', function() {
+        var method = givenRestStaticMethod({
+          returns: { root: true, type: Array }
+        });
+        expect(method.isReturningArray()).to.equal(true);
+      });
+
+      it('returns true when there is single root "array" arg', function() {
+        var method = givenRestStaticMethod({
+          returns: { root: true, type: Array }
+        });
+        expect(method.isReturningArray()).to.equal(true);
+      });
+
+      it('returns false otherwise', function() {
+        var method = givenRestStaticMethod({
+          returns: { arg: 'result', type: Array }
+        });
+        expect(method.isReturningArray()).to.equal(false);
+      });
+    });
+
+    describe('acceptsSingleBodyArgument()', function() {
+      it('returns true when the arg is a single Object from body', function() {
+        var method = givenRestStaticMethod({
+          accepts: {
+            arg: 'data',
+            type: Object,
+            http: { source: 'body' }
+          }
+        });
+        expect(method.acceptsSingleBodyArgument()).to.equal(true);
+      });
+
+      it('returns false otherwise', function() {
+        var method = givenRestStaticMethod({
+          accepts: { arg: 'data', type: Object }
+        });
+        expect(method.acceptsSingleBodyArgument()).to.equal(false);
+      });
+    });
+
+    describe('getHttpMethod', function() {
+      it('returns POST for `all`', function() {
+        var method = givenRestStaticMethod({ http: { verb: 'all'} });
+        expect(method.getHttpMethod()).to.equal('POST');
+      });
+
+      it('returns DELETE for `del`', function() {
+        var method = givenRestStaticMethod({ http: { verb: 'del'} });
+        expect(method.getHttpMethod()).to.equal('DELETE');
+      });
+
+      it('returns upper-case value otherwise', function() {
+        var method = givenRestStaticMethod({ http: { verb: 'get'} });
+        expect(method.getHttpMethod()).to.equal('GET');
+      });
+    });
+
+    describe('getPath', function() {
+      it('returns the path of the first route', function() {
+        var method = givenRestStaticMethod({ http: [
+          { path: '/a-path' },
+          { path: '/another-path' }
+        ]});
+        expect(method.getPath()).to.equal('/a-path');
+      });
+    });
+
+    describe('getFullPath', function() {
+      it('returns class path + method path', function() {
+        var method = givenRestStaticMethod(
+          { http: { path: '/a-method' } },
+          { http: { path: '/a-class' } }
+        );
+
+        expect(method.getFullPath()).to.equal('/a-class/a-method');
+      });
+    });
+
+    function givenRestStaticMethod(methodConfig, classConfig) {
+      var name = 'testMethod';
+      methodConfig = extend({ shared: true }, methodConfig);
+      classConfig = extend({ shared: true}, classConfig);
+      remotes.testClass = extend({}, classConfig);
+      var fn = remotes.testClass[name] = extend(function(){}, methodConfig);
+
+      var sharedClass = new SharedClass('testClass', remotes.testClass);
+      var restClass = new RestAdapter.RestClass(sharedClass);
+
+      var sharedMethod = new SharedMethod(fn, name, sharedClass, true);
+      return new RestAdapter.RestMethod(restClass, sharedMethod);
+    }
+  });
+});
+
+function someFunc() {
+}
