51e2afe0817e9f5df11ef4c388ca79ae9342b117
Ritchie Martori <skawful@gmail.com>
2014-05-16 13:57:54 -0700

    Add APIs for manually adding classes and methods

--- test/shared-class.test.js
+++ test/shared-class.test.js
@@ -2,6 +2,7 @@ var extend = require('util')._extend;
 var expect = require('chai').expect;
 var SharedClass = require('../lib/shared-class');
 var factory = require('./helpers/shared-objects-factory.js');
+var RemoteObjects = require('../');
 
 describe('SharedClass', function() {
   var SomeClass;
@@ -19,4 +20,28 @@ describe('SharedClass', function() {
       expect(sc.http.path).to.equal('/some');
     });
   });
+
+  describe('sharedClass.defineMethod(name, options)', function() {
+    it('defines a remote method', function () {
+      var sc = new SharedClass('SomeClass', SomeClass);
+      SomeClass.prototype.myMethod = function() {};
+      var METHOD_NAME = 'myMethod';
+      sc.defineMethod(METHOD_NAME, {
+        prototype: true
+      });
+      var methods = sc.methods().map(function(m) {return m.name});
+      expect(methods).to.contain(METHOD_NAME);
+    });
+  });
+
+  describe('remotes.addClass(sharedClass)', function() {
+    it('should make the class available', function () {
+      var CLASS_NAME = 'SomeClass';
+      var remotes = RemoteObjects.create();
+      var sharedClass = new SharedClass(CLASS_NAME, SomeClass);
+      remotes.addClass(sharedClass);
+      var classes = remotes.classes().map(function(c) {return c.name});
+      expect(classes).to.contain(CLASS_NAME);
+    });
+  })
 });
