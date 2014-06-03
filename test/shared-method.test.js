51d7816f4bf57b3c6e5f9db28ebbb38e19bd2471
Ritchie Martori <skawful@gmail.com>
2014-06-03 13:39:23 -0700

    Rename willInvoke => isDelegateFor

--- test/shared-method.test.js
+++ test/shared-method.test.js
@@ -4,19 +4,19 @@ var SharedMethod = require('../lib/shared-method');
 var factory = require('./helpers/shared-objects-factory.js');
 
 describe('SharedMethod', function() {
-  describe('sharedMethod.willInvoke(suspect, [isStatic])', function () {
+  describe('sharedMethod.isDelegateFor(suspect, [isStatic])', function () {
     it('check if the given function is going to be invoked', function () {
       var mockSharedClass = {};
       var sharedMethod = new SharedMethod(myFunction, 'myName', mockSharedClass);
-      assert.equal(sharedMethod.willInvoke(myFunction), true);
+      assert.equal(sharedMethod.isDelegateFor(myFunction), true);
 
       function myFunction() {}
     });
     it('check by name if function is going to be invoked', function () {
       var mockSharedClass = {prototype: {myName: myFunction}};
       var sharedMethod = new SharedMethod(myFunction, 'myName', mockSharedClass);
-      assert.equal(sharedMethod.willInvoke('myName', false), true);
-      assert.equal(sharedMethod.willInvoke('myName'), true);
+      assert.equal(sharedMethod.isDelegateFor('myName', false), true);
+      assert.equal(sharedMethod.isDelegateFor('myName'), true);
 
       function myFunction() {}
     });
@@ -24,7 +24,7 @@ describe('SharedMethod', function() {
       var mockSharedClass = {myName: myFunction};
       var options = {isStatic: true};
       var sharedMethod = new SharedMethod(myFunction, 'myName', mockSharedClass, options);
-      assert.equal(sharedMethod.willInvoke('myName', true), true);
+      assert.equal(sharedMethod.isDelegateFor('myName', true), true);
 
       function myFunction() {}
     });
@@ -32,7 +32,7 @@ describe('SharedMethod', function() {
       var mockSharedClass = {myName: myFunction};
       var options = {isStatic: true, aliases: ['myAlias']};
       var sharedMethod = new SharedMethod(myFunction, 'myName', mockSharedClass, options);
-      assert.equal(sharedMethod.willInvoke('myAlias', true), true);
+      assert.equal(sharedMethod.isDelegateFor('myAlias', true), true);
 
       function myFunction() {}
     });
