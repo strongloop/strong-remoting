217e2b4dee0f2d090b5dd0fa6c88cc72dd1c0b69
Ritchie Martori <skawful@gmail.com>
2015-06-10 17:07:27 -0700

    Add object mode ReadableStream support

--- test/streams.js
+++ test/streams.js
@@ -1,8 +1,11 @@
 var assert = require('assert');
 var RemoteObjects = require('../');
+var expect = require('chai').expect;
+var SharedClass = require('../lib/shared-class');
 var express = require('express');
 var request = require('supertest');
 var fs = require('fs');
+var es = require('event-stream');
 
 describe('strong-remoting', function() {
   var app;
@@ -46,5 +49,89 @@ describe('strong-remoting', function() {
 
     json('get', '/fs/createReadStream?path=' + __dirname + '/data/foo.json&encoding=utf8')
       .expect({bar: 'baz'}, done);
+  });
+});
+
+describe('a function returning a ReadableStream', function() {
+  var Readable = require('stream').Readable;
+  var remotes = RemoteObjects.create();
+  var streamClass;
+  var server;
+
+  before(function(done) {
+    var test = this;
+    // NOTE: Date object will not be coerced back into a Date
+    var data = test.data = [{foo: 'bar'}, 'bat', false, 0, null];
+    var app = express();
+    server = app.listen(0, '127.0.0.1', done);
+    function StreamClass() {
+
+    }
+
+    StreamClass.createStream = function createStream(cb) {
+      cb(null, es.readArray(test.data));
+    };
+
+    StreamClass.createStreamWithError = function createStreamWithError(cb) {
+      var rs = new Readable({objectMode: true});
+      rs._read = function() {
+        rs.emit('error', new Error('test error'));
+      };
+      cb(null, rs);
+    };
+
+    streamClass = new SharedClass('StreamClass', StreamClass);
+
+    streamClass.defineMethod('createStream', {
+      isStatic: true,
+      fn: StreamClass.createStream,
+      returns: [{
+        arg: 'stream',
+        type: 'ReadableStream',
+        json: true
+      }]
+    });
+
+    streamClass.defineMethod('createStreamWithError', {
+      isStatic: true,
+      fn: StreamClass.createStreamWithError,
+      returns: [{
+        arg: 'stream',
+        type: 'ReadableStream',
+        json: true
+      }]
+    });
+
+    remotes.addClass(streamClass);
+    app.use(remotes.handler('rest'));
+  });
+
+  before(function() {
+    remotes.connect('http://127.0.0.1:' + server.address().port, 'rest');
+  });
+
+  it('should return a ReadableStream', function(done) {
+    var testData = this.data;
+    remotes.invoke('StreamClass.createStream', [], function(err, stream) {
+      assert(stream.readable, 'must be a ReadableStream');
+      var out = es.writeArray(function (err, result) {
+        if(err) return done(err);
+        expect(testData).to.eql(result);
+        done();
+      });
+
+      stream.pipe(out);
+    });
+  });
+
+  it('should include errors', function(done) {
+    remotes.invoke('StreamClass.createStreamWithError', [], function(err, stream) {
+      stream.on('error', function(err) {
+        expect(err).instanceof(Error);
+        expect(err.message).to.equal('test error');
+      });
+
+      stream.on('end', done);
+    });
   });
 });
