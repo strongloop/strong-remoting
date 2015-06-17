ac18160efdac60ae3eb1b0fb319d83fd52a0cea9
Ritchie Martori <skawful@gmail.com>
2015-06-17 13:36:17 -0700

    Add support for Event Source streams

--- test/streams.js
+++ test/streams.js
@@ -6,6 +6,7 @@ var express = require('express');
 var request = require('supertest');
 var fs = require('fs');
 var es = require('event-stream');
+var EventSource = require('eventsource');
 
 describe('strong-remoting', function() {
   var app;
@@ -57,12 +58,13 @@ describe('a function returning a ReadableStream', function() {
   var remotes = RemoteObjects.create();
   var streamClass;
   var server;
+  var app;
 
   before(function(done) {
     var test = this;
-    // NOTE: Date object will not be coerced back into a Date
+    // NOTE: Date is intentionally excluded as it is not supported yet
     var data = test.data = [{foo: 'bar'}, 'bat', false, 0, null];
-    var app = express();
+    app = express();
     server = app.listen(0, '127.0.0.1', done);
     function StreamClass() {
 
@@ -74,19 +76,24 @@ describe('a function returning a ReadableStream', function() {
 
     StreamClass.createStreamWithError = function createStreamWithError(cb) {
       var rs = new Readable({objectMode: true});
+
       rs._read = function() {
-        rs.emit('error', new Error('test error'));
+        // required method
       };
+
+      process.nextTick(function() {
+        rs.emit('error', new Error('test error'));
+      });
       cb(null, rs);
     };
 
     streamClass = new SharedClass('StreamClass', StreamClass);
 
-    streamClass.defineMethod('createStream', {
+    this.createStreamMethod = streamClass.defineMethod('createStream', {
       isStatic: true,
       fn: StreamClass.createStream,
       returns: [{
-        arg: 'stream',
+        arg: 'result',
         type: 'ReadableStream',
         json: true
       }]
@@ -96,7 +103,7 @@ describe('a function returning a ReadableStream', function() {
       isStatic: true,
       fn: StreamClass.createStreamWithError,
       returns: [{
-        arg: 'stream',
+        arg: 'result',
         type: 'ReadableStream',
         json: true
       }]
@@ -114,8 +121,8 @@ describe('a function returning a ReadableStream', function() {
     var testData = this.data;
     remotes.invoke('StreamClass.createStream', [], function(err, stream) {
       assert(stream.readable, 'must be a ReadableStream');
-      var out = es.writeArray(function (err, result) {
-        if(err) return done(err);
+      var out = es.writeArray(function(err, result) {
+        if (err) return done(err);
         expect(testData).to.eql(result);
         done();
       });
@@ -132,6 +139,51 @@ describe('a function returning a ReadableStream', function() {
       });
 
       stream.on('end', done);
+    });
+  });
+
+  describe('an http client requesting a stream as an event source', function() {
+    before(function(done) {
+      var server = this.server = app.listen(done);
+      this.port = server.address().port;
+    });
+    before(function() {
+      var test = this;
+      this.url = 'http://localhost:' + this.port;
+    });
+
+    it('should respond with an event stream', function(done) {
+      var es = new EventSource(this.url + '/StreamClass/createStream');
+      var testData = this.data;
+      var result = [];
+
+      es.on('data', function(e) {
+        result.push(JSON.parse(e.data));
+      });
+
+      es.on('end', function() {
+        expect(testData).to.eql(result);
+        done();
+      });
+    });
+
+    it('should respond with an event stream with errors', function(done) {
+      var es = new EventSource(this.url + '/StreamClass/createStreamWithError');
+
+      es.on('error', function(e) {
+        var err;
+        if (e && e.data) {
+          err = JSON.parse(e.data);
+        } else {
+          return done(new Error('no error data!'));
+        }
+        expect(err.message).to.equal('test error');
+        done();
+      });
+    });
+
+    after(function() {
+      this.server.close();
     });
   });
 });
