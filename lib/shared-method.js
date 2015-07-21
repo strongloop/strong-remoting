strongloop/strong-remoting

commit 4d965966f4b860c27a6a42c421c7ea7e9d3696f5

Clean up and fix API docs

lib/shared-method.js
@@ -4,64 +4,66 @@
 
 module.exports = SharedMethod;
 
-/*!
- * Module dependencies.
- */
-
 var debug = require('debug')('strong-remoting:shared-method');
 var util = require('util');
 var traverse = require('traverse');
 var assert = require('assert');
 
 /**
- * Create a new `SharedMethod` with the given `fn`.
+ * Create a new `SharedMethod` (remote method) with the given `fn`.
+ * See also [Remote methods](http://docs.strongloop.com/display/LB/Remote+methods).
  *
- * @class SharedMethod
- * @param {Function} fn The `Function` to be invoked when the method is invoked
- * @param {String} name The name of the `SharedMethod`
- * @param {SharedClass} sharedClass The `SharedClass` the method will be attached to
- * @param {Object|Boolean} options
- * @param {Boolean} [options.isStatic] Is the method a static method or a
- * a `prototype` method
- * @param {Array} [options.aliases] A list of aliases for the
- * @param {Array} [options.http] HTTP only options
- * @param {Number} [options.http.status] The default status code
- * @param {Number} [options.http.errorStatus] The default error status code
- * `sharedMethod.name`
- * @param {Array|Object} [options.accepts] An `Array` of argument definitions
- * that describe the arguments of the `SharedMethod`.
- * @param {Boolean} [options.shared] Default is `true`
- * @param {String} [options.accepts.arg] The name of the argument
- * @param {String} [options.accepts.http] HTTP mapping for the argument
- * @param {String} [options.accepts.http.source] The HTTP source for the
- * argument. May be one of the following:
- *
- * - `req` - the Express `Request` object
- * - `req` - the Express `Request` object
- * - `body` - the `req.body` value
- * - `form` - `req.body[argumentName]`
- * - `query` - `req.query[argumentName]`
- * - `path` - `req.params[argumentName]`
- * - `header` - `req.headers[argumentName]`
- * - `context` - the current `HttpContext`
- * @param {Object} [options.accepts.rest] The REST mapping / settings for the
- * argument.
- * @param {Array|Object} [options.returns] An `Array` of argument definitions
- * @param {Array|Object} [options.errors] An `Array` of error definitions
- * The same options are available as `options.accepts`.
- * @property {String} name The method name
- * @property {String[]} aliases An array of method aliases
- * @property {Array|Object} isStatic
- * @property {Array|Object} accepts See `options.accepts`
- * @property {Array|Object} returns See `options.returns`
- * @property {Array|Object} errors See `options.errors`
- * @property {String} description
- * @property {String} notes
+ * @property {String} name The method name.
+ * @property {String[]} aliases An array of method aliases.
+ * @property {Boolean} isStatic Whether the method is static; from `options.isStatic`.  Default is `true`.
+ * @property {Array|Object} accepts See `options.accepts`.
+ * @property {Array|Object} returns See `options.returns`.
+ * @property {Array|Object} errors See `options.errors`.
+ * @property {String} description Text description of the method.
+ * @property {String} notes Additional notes, used by API documentation generators like Swagger.
  * @property {String} http
  * @property {String} rest
  * @property {String} shared
- * @property {Boolean} [documented] Default: true. Set to `false` to exclude
- *   the method from Swagger metadata.
+ * @property {Boolean} [documented] Default: true. Set to `false` to exclude the method from Swagger metadata.
+ *
+ * @param {Function} fn The `Function` to be invoked when the method is invoked.
+ * @param {String} name The name of the `SharedMethod`.
+ * @param {SharedClass} sharedClass The `SharedClass` to which the method will be attached.
+ *
+ * @options {Object} options See below.
+ * @property {Array|Object} [accepts] Defines either a single argument as an object or an ordered set of arguments as an array.
+ * @property {String} [accepts.arg] The name of the argument.
+ * @property {String} [accepts.description] Text description of the argument, used by API documentation generators like Swagger.
+ * @property {String} [accepts.http] HTTP mapping for the argument.
+ * See [HTTP mapping of input arguments](http://docs.strongloop.com/display/LB/Remote+methods#Remotemethods-HTTPmappingofinputarguments).
+ * @property {String} [accepts.http.source] The HTTP source for the  argument. May be one of the following:
+ *
+ * - `req` - the Express `Request` object.
+ * - `res` - the Express `Response` object.
+ * - `body` - the `req.body` value.
+ * - `form` - `req.body[argumentName]`.
+ * - `query` - `req.query[argumentName]`.
+ * - `path` - `req.params[argumentName]`.
+ * - `header` - `req.headers[argumentName]`.
+ * - `context` - the current `HttpContext`.
+ * @property {Object} [accepts.rest] The REST mapping / settings for the argument.
+ * @property {String} [accepts.type] Argument datatype; must be a [Loopback type](http://docs.strongloop.com/display/LB/LoopBack+types).
+ * @property {Array} [aliases] A list of aliases for the method.
+ * @property {Array|Object} [errors] Object or `Array` containing error definitions.
+ * @property {Array} [http] HTTP-only options.
+ * @property {Number} [http.errorStatus] Default error status code.
+ * @property {String} [http.path] HTTP path (relative to the model) at which the method is exposed.
+ * @property {Number} [http.status] Default status code when the callback is called _without_ an error.
+ * @property {String} [http.verb] HTTP method (verb) at which the method is available. One of:
+ * get, post (default), put, del, or all
+ * @property {Boolean} [isStatic] Whether the method is a static method or a prototype method.
+ * @property {Array|Object} [returns] Specifies the remote method's callback arguments; either a single argument as an object or an ordered set of arguments as an array.
+ * The `err` argument is assumed; do not specify.  NOTE: Can have the same properties as `accepts`, except for `http.target`.
+ * @property {Boolean} [shared] Whether the method is shared.  Default is `true`.
+ * @property {Number} [status] The default status code.
+ * @end
+ *
+ * @class
  */
 
 function SharedMethod(fn, name, sc, options) {
@@ -547,10 +549,10 @@ SharedMethod.prototype.getFunction = function() {
 };
 
 /**
- * Will this shared method invoke the given `suspect`?
+ * Determine if this shared method invokes the given "suspect" function.
  *
+ * @example
  * ```js
- * // examples
  * sharedMethod.isDelegateFor(myClass.myMethod); // pass a function
  * sharedMethod.isDelegateFor(myClass.prototype.myInstMethod);
  * sharedMethod.isDelegateFor('myMethod', true); // check for a static method by name
@@ -559,6 +561,7 @@ SharedMethod.prototype.getFunction = function() {
  *
  * @param {String|Function} suspect The name of the suspected function
  * or a `Function`.
+ * @returns Boolean True if the shared method invokes the given function; false otherwise.
  */
 
 SharedMethod.prototype.isDelegateFor = function(suspect, isStatic) {
@@ -581,7 +584,7 @@ SharedMethod.prototype.isDelegateFor = function(suspect, isStatic) {
 /**
  * Add an alias
  *
- * @param {String} alias
+ * @param {String} alias Alias method name.
  */
 
 SharedMethod.prototype.addAlias = function(alias) {

lib/http-context.js
@@ -18,16 +18,24 @@ var js2xmlparser = require('js2xmlparser');
 var DEFAULT_SUPPORTED_TYPES = [
     'application/json', 'application/javascript', 'application/xml',
     'text/javascript', 'text/xml',
-    'json', 'xml',
+    'json', 'xml' ,
     '*/*'
-  ];
+    ];
+
+/*!
+ * This comment is here as a workaround for a strong-docs bug.
+ * The above array value leads to spurious doc output.
+ */
 
 /**
  * Create a new `HttpContext` with the given `options`.
  *
- * @param {Object} options
- * @return {HttpContext}
- * @class HttpContext
+ * @param {Object} req Express Request object.
+ * @param {Object} res Express Response object.
+ * @param {Function} method  ?
+ * @options {Object} options See below.
+ * @property {Boolean} xml Set to `true` to enable XML-based types.  Default is false.
+ * @class
  */
 
 function HttpContext(req, res, method, options) {
@@ -50,7 +58,7 @@ function HttpContext(req, res, method, options) {
   req.remotingContext = this;
 }
 
-/**
+/*!
  * Inherit from `EventEmitter`.
  */
 

lib/http-invocation.js
@@ -205,9 +205,10 @@ HttpInvocation.prototype.createRequest = function() {
 };
 
 /**
- * Get an arg value by name using the given options.
+ * Get an argument value by name.
  *
  * @param {String} name
+ * @returns {*} Value of specified argument.
  */
 
 HttpInvocation.prototype.getArgByName = function(name) {

lib/remote-objects.js
@@ -195,13 +195,11 @@ RemoteObjects.prototype.addClass = function(sharedClass) {
 /**
  * Find a method by its string name.
  *
- * Example Method Strings:
+ * @param {String} methodString String specifying the method. For example:
  *
  *  - `MyClass.prototype.myMethod`
  *  - `MyClass.staticMethod`
  *  - `obj.method`
- *
- * @param {String} methodString
  */
 
 RemoteObjects.prototype.findMethod = function(methodString) {
@@ -251,10 +249,10 @@ RemoteObjects.prototype.toJSON = function() {
 /**
  * Execute the given function before the matched method string.
  *
- * **Examples:**
+ * @example
  *
+ * Do something before the `user.greet` method is called:
  * ```js
- * // Do something before our `user.greet` example, earlier.
  * remotes.before('user.greet', function(ctx, next) {
  *   if ((ctx.req.param('password') || '').toString() !== '1234') {
  *     next(new Error('Bad password!'));
@@ -262,14 +260,18 @@ RemoteObjects.prototype.toJSON = function() {
  *     next();
  *   }
  * });
+ * ```
  *
- * // Do something before any `user` method.
+ * Do something before any `user` method:
+ * ```js
  * remotes.before('user.*', function(ctx, next) {
  *   console.log('Calling a user method.');
  *   next();
  * });
+ * ```
  *
- * // Do something before a `dog` instance method.
+ * Do something before a `dog` instance method:
+ * ```js
  * remotes.before('dog.prototype.*', function(ctx, next) {
  *   var dog = this;
  *   console.log('Calling a method on "%s".', dog.name);
@@ -291,23 +293,27 @@ RemoteObjects.prototype.before = function(methodMatch, fn) {
 /**
  * Execute the given `hook` function after the matched method string.
  *
- * **Examples:**
+ * @example
  *
+ * Do something after the `speak` instance method.
+ * NOTE: you cannot cancel a method after it has been called.
  * ```js
- * // Do something after the `speak` instance method.
- * // NOTE: you cannot cancel a method after it has been called.
  * remotes.after('dog.prototype.speak', function(ctx, next) {
  *   console.log('After speak!');
  *   next();
  * });
+ *```
  *
- * // Do something before all methods.
+ * Do something before all methods.
+ ```js
  * remotes.before('**', function(ctx, next, method) {
  *   console.log('Calling:', method.name);
  *   next();
  * });
+ * ```
  *
- * // Modify all returned values named `result`.
+ * Modify all returned values named `result`.
+ * ```js
  * remotes.after('**', function(ctx, next) {
  *   ctx.result += '!!!';
  *   next();
@@ -329,29 +335,35 @@ RemoteObjects.prototype.after = function(methodMatch, fn) {
  * Execute the given `hook` function after the method matched by the method
  * string failed.
  *
- * **Examples:**
+ * @example
+ * Do something after the `speak` instance method failed.
  *
  * ```js
- * // Do something after the `speak` instance method failed.
  * remotes.afterError('dog.prototype.speak', function(ctx, next) {
  *   console.log('Cannot speak!', ctx.error);
  *   next();
  * });
+ * ```
  *
- * // Do something before all methods.
+ * Do something before all methods:
+ * ```js
  * remotes.afterError('**', function(ctx, next, method) {
  *   console.log('Failed', method.name, ctx.error);
  *   next();
  * });
+ * ```
  *
- * // Modify all returned errors
+ * Modify all returned errors:
+ * ```js
  * remotes.after('**', function(ctx, next) {
  *   if (!ctx.error.details) ctx.result.details = {};
  *   ctx.error.details.info = 'intercepted by a hook';
  *   next();
  * });
+ * ```
  *
- * // Report a different error
+ * Report a different error:
+ * ```js
  * remotes.after('dog.prototype.speak', function(ctx, next) {
  *   console.error(ctx.error);
  *   next(new Error('See server console log for details.'));

lib/shared-class.js
@@ -1,10 +1,10 @@
-/**
+/*!
  * Expose `SharedClass`.
  */
 
 module.exports = SharedClass;
 
-/**
+/*!
  * Module dependencies.
  */
 
@@ -18,13 +18,12 @@ var assert = require('assert');
 /**
  * Create a new `SharedClass` with the given `options`.
  *
- * @class SharedClass
  * @param {String} name The `SharedClass` name
  * @param {Function} constructor The `constructor` the `SharedClass` represents
  * @param {Object} options Additional options.
  * @property {Function} ctor The `constructor`
  * @property {Object} http The HTTP settings
- * @return {SharedClass}
+ * @class
  */
 
 function SharedClass(name, ctor, options) {
@@ -70,7 +69,7 @@ function SharedClass(name, ctor, options) {
 }
 
 /**
- * Normalize http path.
+ * Normalize HTTP path.
  */
 
 SharedClass.normalizeHttpPath = function(path) {
@@ -144,7 +143,7 @@ SharedClass.prototype.isMethodEnabled = function(sharedMethod) {
  *
  * @param {String} name The method name
  * @param {Object} [options] Set of options used to create a `SharedMethod`.
- * [See the full set of options](#sharedmethod-new-sharedmethodfn-name-sharedclass-options)
+ * [See the full set of options](#sharedmethod)
  */
 
 SharedClass.prototype.defineMethod = function(name, options, fn) {
@@ -161,6 +160,7 @@ function _define(methods, name, options, fn) {
 /**
  * Define a shared method resolver for dynamically defining methods.
  *
+ * @example
  * ```js
  * // below is a simple example
  * sharedClass.resolve(function(define) {
@@ -175,7 +175,7 @@ function _define(methods, name, options, fn) {
  * }
  * ```
  *
- * @param {Function} resolver
+ * @param {Function} resolver The resolver function.
  */
 
 SharedClass.prototype.resolve = function(resolver) {
@@ -212,8 +212,8 @@ SharedClass.prototype.disableMethod = function(fn, isStatic) {
 /**
  * Get a key for the given method.
  *
- * @param {String} fn The function or method name
- * @param {Boolean} isStatic
+ * @param {String} fn The function or method name.
+ * @param {Boolean} isStatic True if the method is static.
  */
 
 SharedClass.prototype.getKeyFromMethodNameAndTarget = function(name, isStatic) {

commit 97eddc2af0432750800944ee1acada83236cba46

fix long lines in docs that were failing jshint
Introduced in 4d96596

lib/shared-method.js
@@ -15,28 +15,34 @@ var assert = require('assert');
  *
  * @property {String} name The method name.
  * @property {String[]} aliases An array of method aliases.
- * @property {Boolean} isStatic Whether the method is static; from `options.isStatic`.  Default is `true`.
+ * @property {Boolean} isStatic Whether the method is static; from `options.isStatic`.
+ * Default is `true`.
  * @property {Array|Object} accepts See `options.accepts`.
  * @property {Array|Object} returns See `options.returns`.
  * @property {Array|Object} errors See `options.errors`.
  * @property {String} description Text description of the method.
- * @property {String} notes Additional notes, used by API documentation generators like Swagger.
+ * @property {String} notes Additional notes, used by API documentation generators like
+ * Swagger.
  * @property {String} http
  * @property {String} rest
  * @property {String} shared
- * @property {Boolean} [documented] Default: true. Set to `false` to exclude the method from Swagger metadata.
+ * @property {Boolean} [documented] Default: true. Set to `false` to exclude the method
+ * from Swagger metadata.
  *
  * @param {Function} fn The `Function` to be invoked when the method is invoked.
  * @param {String} name The name of the `SharedMethod`.
  * @param {SharedClass} sharedClass The `SharedClass` to which the method will be attached.
  *
  * @options {Object} options See below.
- * @property {Array|Object} [accepts] Defines either a single argument as an object or an ordered set of arguments as an array.
+ * @property {Array|Object} [accepts] Defines either a single argument as an object or an
+ * ordered set of arguments as an array.
  * @property {String} [accepts.arg] The name of the argument.
- * @property {String} [accepts.description] Text description of the argument, used by API documentation generators like Swagger.
- * @property {String} [accepts.http] HTTP mapping for the argument.
- * See [HTTP mapping of input arguments](http://docs.strongloop.com/display/LB/Remote+methods#Remotemethods-HTTPmappingofinputarguments).
- * @property {String} [accepts.http.source] The HTTP source for the  argument. May be one of the following:
+ * @property {String} [accepts.description] Text description of the argument, used by API
+ * documentation generators like Swagger.
+ * @property {String} [accepts.http] HTTP mapping for the argument. See argument mapping in
+ * [the docs](http://docs.strongloop.com/x/-Yw6#Remotemethods-HTTPmappingofinputarguments).
+ * @property {String} [accepts.http.source] The HTTP source for the  argument. May be one
+ * of the following:
  *
  * - `req` - the Express `Request` object.
  * - `res` - the Express `Response` object.
@@ -47,18 +53,24 @@ var assert = require('assert');
  * - `header` - `req.headers[argumentName]`.
  * - `context` - the current `HttpContext`.
  * @property {Object} [accepts.rest] The REST mapping / settings for the argument.
- * @property {String} [accepts.type] Argument datatype; must be a [Loopback type](http://docs.strongloop.com/display/LB/LoopBack+types).
+ * @property {String} [accepts.type] Argument datatype; must be a
+ * [Loopback type](http://docs.strongloop.com/display/LB/LoopBack+types).
  * @property {Array} [aliases] A list of aliases for the method.
  * @property {Array|Object} [errors] Object or `Array` containing error definitions.
  * @property {Array} [http] HTTP-only options.
  * @property {Number} [http.errorStatus] Default error status code.
- * @property {String} [http.path] HTTP path (relative to the model) at which the method is exposed.
- * @property {Number} [http.status] Default status code when the callback is called _without_ an error.
- * @property {String} [http.verb] HTTP method (verb) at which the method is available. One of:
- * get, post (default), put, del, or all
- * @property {Boolean} [isStatic] Whether the method is a static method or a prototype method.
- * @property {Array|Object} [returns] Specifies the remote method's callback arguments; either a single argument as an object or an ordered set of arguments as an array.
- * The `err` argument is assumed; do not specify.  NOTE: Can have the same properties as `accepts`, except for `http.target`.
+ * @property {String} [http.path] HTTP path (relative to the model) at which the method is
+ * exposed.
+ * @property {Number} [http.status] Default status code when the callback is called
+ * _without_ an error.
+ * @property {String} [http.verb] HTTP method (verb) at which the method is available.
+ * One of: get, post (default), put, del, or all
+ * @property {Boolean} [isStatic] Whether the method is a static method or a prototype
+ * method.
+ * @property {Array|Object} [returns] Specifies the remote method's callback arguments;
+ * either a single argument as an object or an ordered set of arguments as an array.
+ * The `err` argument is assumed; do not specify.  NOTE: Can have the same properties as
+ * `accepts`, except for `http.target`.
  * @property {Boolean} [shared] Whether the method is shared.  Default is `true`.
  * @property {Number} [status] The default status code.
  * @end