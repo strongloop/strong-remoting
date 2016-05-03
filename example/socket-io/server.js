aa391d2e65460196ae2a59493f9dd4844622f35d
Ryan Graham <r.m.graham@gmail.com>
2016-05-03 16:06:13 -0700

    update copyright notices

--- example/socket-io/server.js
+++ example/socket-io/server.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2013,2016. All Rights Reserved.
+// Node module: strong-remoting
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
 // create a set of shared classes
 var remotes = require('../../').create();
 
