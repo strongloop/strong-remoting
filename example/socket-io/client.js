79999e721b3b5bc3fe08c72c18e9aff9485b7020
deepakrkris <deepak.r.kris@gmail.com>
2016-08-05 14:29:55 -0700

    Add globalization
    strong-globalize changes for this module

--- example/socket-io/client.js
+++ example/socket-io/client.js
@@ -8,9 +8,9 @@ var SocketIOAdapter = require('../../client/js/socket-io-adapter');
 var remotes = Remotes.connect('http://localhost:3000', SocketIOAdapter);
 
 remotes.invoke('fs.readFile', { path: 'test.txt' }, function(err, data) {
-  console.log(data.toString());
+  g.log(data.toString());
 });
 
 remotes.invoke('ee.on', { event: 'foo' }, function(err, data) {
-  console.log('foo event ran!', data); // logged multiple times
+  g.log('foo event ran! %s', data); // logged multiple times
 });
