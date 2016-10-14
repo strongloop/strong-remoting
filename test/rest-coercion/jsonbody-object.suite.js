e9b7977cfb72de32e89ecc03b68a644caec37b7b
David Cheung <davidcheung@ca.ibm.com>
2016-10-14 14:23:15 -0400

    allowArray flag for remoteObject `accept`
    In loopback 2.x endpoints supported batch create which passes
    an Array of Objects into the endpoint that expects Objects,
    this is nolonger allowed in 3.x's stricter coercion
    In order to maintain backwards compatiability, this is a short term
    fix to allow endpoints to accept Array of Objects with the allowArray

--- test/rest-coercion/jsonbody-object.suite.js
+++ test/rest-coercion/jsonbody-object.suite.js
@@ -75,4 +75,25 @@ module.exports = function(ctx) {
       [[1, 2], ERROR_BAD_REQUEST],
     ]);
   });
+
+  describe('json body - object - allowArray: true', function() {
+    verifyTestCases({ arg: 'data', type: 'object', allowArray: true }, [
+      // normal objects is valid
+      [{ x: '' }],
+      [{ x: null }],
+      [{ x: {}}],
+      [{ x: { key: null }}],
+
+      // array of objects also valid
+      [[{}]],
+      [[{ x: '' }]],
+      [[{ x: null }]],
+      [[{ x: 1 }, { y: 'string' }]],
+
+      // array of non-objects are invalid
+      [[{}, [{}]], ERROR_BAD_REQUEST],
+      [[{}, 3.1415], ERROR_BAD_REQUEST],
+      [[{}, 'non-object'], ERROR_BAD_REQUEST],
+    ]);
+  });
 };
